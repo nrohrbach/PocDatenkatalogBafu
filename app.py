43d6567836823da1b9030dda244c1759371874dc
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2025-04-02 23:05:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 # Definiere die Liste der Optionen
-options = ['Abfall',
+options = ['',
+            'Abfall',
            'Altlasten',
            'Bildung, Forschung, Innovation',
            'Biodiversität',
@@ -85,12 +86,66 @@ def search_datasets(keyword):
 
     return dfOpendataSwiss
 
+# Indikatoren nach Thema filtern
+def filter_df_by_thema(thema):
+    url = "https://www.bafu.admin.ch/bafu/de/home/zustand/indikatoren.html"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for bad status codes
+     
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching URL: {e}")
+        return None
+
+    try:
+      html_content = response.content.decode("utf-8")  # Falls `response.content` Byte-Daten enthält.
+      soup = BeautifulSoup(html_content, "html.parser")
+      table_body = soup.find("table", attrs={"class": "table"})
+
+      if not table_body:
+        print("Table not found on the webpage")
+        return None
+
+      rows = table_body.find_all("tr")
+      data = []
+      for row in rows:
+          cells = row.find_all("td")
+          if len(cells) >= 2:
+              thema_cell = cells[0].text.strip()
+              indikatorname = cells[1].text.strip()
+              link_element = cells[1].find('a')
+              href = link_element.get('href') if link_element else None
+              data.append({"Thema": thema_cell, "Indikatorname": indikatorname, "Link": "https://www.bafu.admin.ch" + href if href else None})
+
+      df = pd.DataFrame(data)
+    except Exception as e:
+      print(f"An error occurred while processing the webpage: {e}")
+      return None
+
+    # Filter the DataFrame using .isin() for multiple Thema values if needed
+    if isinstance(thema, list):
+      filtered_df = df[df["Thema"].isin(thema)]
+    else:
+      filtered_df = df[df["Thema"] == thema]
+
+    return filtered_df
+
+
+
+
+
+
 
 st.title("PoC BAFU Datenkatalog")
 
 st.markdown(
     """
-Beschreibung
+Proof of Concept wie aus der API von opendata.swiss ein Datenkatalog für das BAFU erstellt werden kann.
+In folgenden Themen sind bereits Datensätze verschlagwortet:
+- Wald
+- Boden
+- Klima
+- Luft
 
 """
 )
@@ -105,10 +160,21 @@ mapped_option = map_options(selected_option)
 # Verwende die Abfrage opendata.swiss mit dem gemappten Thema
 dfOGD = search_datasets(mapped_option)
 
+#dfIndikator = filter_df_by_thema(selected_option)
+
+
 #Antwort
-st.subheader("OGD Publikationen des BAFU zum Thema")
-st.dataframe(dfOGD)
+st.subheader("OGD Publikationen des BAFU zum Thema opendata.swiss")
 
 # Zeige die gemappte Option an
 if mapped_option:
-    st.write(f"Du hast {selected_option} ausgewählt, das gemappte Thema ist: {mapped_option}.")
+    st.write(f"Das BAFU publiziert folgende Daten zum Thema {selected_option}:")
+
+for index, row in dfOGD.iterrows():
+    with st.expander(f"**{row['title']}**"):
+        st.write(f"{row['description']}")
+        st.markdown(f"[{row['title0']}]({row['url0']})")
+        st.markdown(f"[{row['title1']}]({row['url1']})")
+
+#st.subheader("OGD Publikationen des BAFU zum Thema Indikator")
+#st.dataframe(dfIndikator)
