7897291bee581f125fa220e88107b62b0599aa9c
Kronmar-Bafu <marco.kronenberg@bafu.admin.ch>
2025-07-23 10:47:02 +0200

    added optional modified

--- convert_dcat.py
+++ convert_dcat.py
@@ -12,7 +12,7 @@ def write_dataset(row: pd.DataFrame, identifier: str):
         'de': row['title']
     }
     dataset['dct:description'] = {
-        'de': row['description']
+        'de': row['description'] if row['description'] is not np.nan else ""
     }
     dataset['dcat:theme'] = "environment"
     dataset['dcat:keyword'] = row['keywords']
@@ -23,8 +23,8 @@ def write_dataset(row: pd.DataFrame, identifier: str):
         "schema:name": row["Kontakt"],
         "schema:email": row["Kontakt"]
     }
-    dataset["dct:issued"] = row["modified"]
-    dataset["dct:modified"] = row["modified"]
+    dataset["dct:issued"] = row["modified"] if row["modified"] is not np.nan else "2025-07-23"
+    dataset["dct:modified"] = row["modified"] if row["modified"] is not np.nan else "2025-07-23"
     dataset["accrualPeriodicity"] = "YEARLY"
     dataset["dct:temporal"] = {
         "dcat:start_date": "1970-01-01",
@@ -35,14 +35,14 @@ def write_dataset(row: pd.DataFrame, identifier: str):
         "dct:identifier": identifier + "dist_a",
         "dct:title": row["title"] + " - Fake Distribution",
         "dct:format": "JSON",
-        "dct:modified": row["modified"]
+        "dct:modified": row["modified"] if row["modified"] is not np.nan else "2025-07-23"
     })
     if row["URL"] is not np.nan:
         dataset["dcat:distribution"].append({
             "dct:identifier": identifier + "dist_indicator",
             "dct:title": row["title"] + " - Indikator",
             "dct:format": "CSV",
-            "dct:modified": row["modified"],
+            "dct:modified": row["modified"] if row["modified"] is not np.nan else "2025-07-23",
             "dcat:accessURL": row["URL"]
         })
     return dataset
